445855124d4f89d4f09da9bdfb3346204f0957bc
Adarsh Raj <iamadarsh008@gmail.com>
2026-04-17 11:22:16 +0530

    subaaray

--- 01_Array/01_MaxSubarray.cpp
+++ 01_Array/01_MaxSubarray.cpp
@@ -1,11 +1,47 @@
 #include<iostream>
-using namespace std;
+#include<vector>
+#include<math.h>
+#include <bits/stdc++.h>
 
+using namespace  std;
+
+// git add.
+// git commit -m "message"
+// git push -u origin main
 
 int main()
 {
-    int arr[5] = {4,-1,2,-5,10};
+    // vector<int> sum_store;
+    int ans = INT_MIN;
+    int arr[5] = {4,-1,2,-5,1};
+    int n = 5;
+    int sum = 0;
+    for(int st=0; st < n; st++)
+    {
+        // int sum = 0;
+        for(int end=st; end < n; end++)
+        {
+            for(int i=st; i<=end; i++)
+                sum += arr[i];
+            
+            ans = max(ans,sum);
+            sum = 0;
+        }
+    }
+    cout << "answer is " << ans;
 
-    
+    // // ********* OPTIMIZE OF BRUTE FORCE *******
+    // for(int st=0; st < n; st++)
+    // {
+    //     int sum = 0;
+    //     for(int end=st; end < n; end++)
+    //     {
+    //         sum += arr[end];
+    //         ans = max(ans,sum);
+    //     }
+    // }
+    // cout << "answer is " << ans;
+
+
     return 0;
 }
